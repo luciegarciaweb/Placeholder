275b31c9d65ba00f5912a6492460eb51842b119e
lucie garcia <lucie2@beweb-16.546O>
2018-06-14 09:48:18 +0200

    blog index created

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Blog</title>
+    </head>
+    <body>
+
+        <h2>Welcome on Ated</h2
+
+        <p><img src='' alt='ated' class='accueil' ></p>
+
+</body>
+</html>
